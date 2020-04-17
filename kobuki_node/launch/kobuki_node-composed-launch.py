973ef1e627c7a06058aaf1ead1fdb24d04988baf
Chris Lalancette <clalancette@openrobotics.org>
2020-04-17 00:27:36 +0000

    Add in launch files.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

---
+++ kobuki_node/launch/kobuki_node-composed-launch.py
@@ -0,0 +1,35 @@
+import os
+
+import ament_index_python.packages
+
+from launch import LaunchDescription
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+import yaml
+
+
+def generate_launch_description():
+    share_dir = ament_index_python.packages.get_package_share_directory('kobuki_node')
+    # Passing parameters to a composed node must be done via a dictionary of
+    # key -> value pairs.  Here we read in the data from the configuration file
+    # and create a dictionary of it that the ComposableNode will accept.
+    params_file = os.path.join(share_dir, 'config', 'kobuki_node_params.yaml')
+    with open(params_file, 'r') as f:
+        params = yaml.safe_load(f)['kobuki_ros_node']['ros__parameters']
+    container = ComposableNodeContainer(
+            node_name='kobuki_node_container',
+            node_namespace='',
+            package='rclcpp_components',
+            node_executable='component_container',
+            composable_node_descriptions=[
+                ComposableNode(
+                    package='kobuki_node',
+                    node_plugin='kobuki_node::KobukiRos',
+                    node_name='kobuki_ros_node',
+                    parameters=[params]),
+            ],
+            output='both',
+    )
+
+    return LaunchDescription([container])
---
+++ kobuki_node/launch/kobuki_node-launch.py
@@ -0,0 +1,26 @@
+import os
+
+import ament_index_python.packages
+import launch
+import launch_ros.actions
+
+import yaml
+
+
+def generate_launch_description():
+    share_dir = ament_index_python.packages.get_package_share_directory('kobuki_node')
+    # There are two different ways to pass parameters to a non-composed node;
+    # either by specifying the path to the file containing the parameters, or by
+    # passing a dictionary containing the key -> value pairs of the parameters.
+    # When starting a *composed* node on the other hand, only the dictionary
+    # style is supported.  To keep the code between the non-composed and
+    # composed launch file similar, we use that style here as well.
+    params_file = os.path.join(share_dir, 'config', 'kobuki_node_params.yaml')
+    with open(params_file, 'r') as f:
+        params = yaml.safe_load(f)['kobuki_ros_node']['ros__parameters']
+    kobuki_ros_node = launch_ros.actions.Node(package='kobuki_node',
+                                              node_executable='kobuki_ros_node',
+                                              output='both',
+                                              parameters=[params])
+
+    return launch.LaunchDescription([kobuki_ros_node])
