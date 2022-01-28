f9842ca60a9b771efde4acbf7055c0639e066d11
Chris Lalancette <clalancette@openrobotics.org>
2022-01-28 08:51:02 -0500

    Port bumper2pc to ROS 2. (#25)
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- kobuki_bumper2pc/src/kobuki_bumper2pc.cpp
+++ kobuki_bumper2pc/src/kobuki_bumper2pc.cpp
@@ -11,29 +11,46 @@
 ** Includes
 *****************************************************************************/
 
-#include <pluginlib/class_list_macros.h>
+#include <cmath>
+#include <functional>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <rcl_interfaces/msg/set_parameters_result.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_components/register_node_macro.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <sensor_msgs/msg/point_field.hpp>
+
+#include "kobuki_ros_interfaces/msg/sensor_state.hpp"
 
 #include "kobuki_bumper2pc/kobuki_bumper2pc.hpp"
 
 namespace kobuki_bumper2pc
 {
 
-void Bumper2PcNodelet::coreSensorCB(const kobuki_msgs::SensorState::ConstPtr& msg)
+void Bumper2PcNode::coreSensorCB(const std::shared_ptr<kobuki_ros_interfaces::msg::SensorState> msg)
 {
-  if (pointcloud_pub_.getNumSubscribers() == 0)
+  if (pointcloud_pub_->get_subscription_count() == 0 &&
+    pointcloud_pub_->get_intra_process_subscription_count() == 0)   // no one listening?
+  {
     return;
+  }
 
   // We publish just one "no events" pc (with all three points far away) and stop spamming when bumper/cliff conditions disappear
-  if (! msg->bumper && ! msg->cliff && ! prev_bumper && ! prev_cliff)
+  if (! msg->bumper && ! msg->cliff && ! prev_bumper_ && ! prev_cliff_)
+  {
     return;
+  }
 
-  prev_bumper = msg->bumper;
-  prev_cliff  = msg->cliff;
+  prev_bumper_ = msg->bumper;
+  prev_cliff_  = msg->cliff;
 
   // We replicate the sensors order of bumper/cliff event messages: LEFT = 0, CENTER = 1 and RIGHT = 2
-  // For any of {left/center/right} with no bumper/cliff event, we publish a faraway point that won't get used 
-  if ((msg->bumper & kobuki_msgs::SensorState::BUMPER_LEFT) ||
-      (msg->cliff  & kobuki_msgs::SensorState::CLIFF_LEFT))
+  // For any of {left/center/right} with no bumper/cliff event, we publish a faraway point that won't get used
+  if ((msg->bumper & kobuki_ros_interfaces::msg::SensorState::BUMPER_LEFT) ||
+      (msg->cliff  & kobuki_ros_interfaces::msg::SensorState::CLIFF_LEFT))
   {
     memcpy(&pointcloud_.data[0 * pointcloud_.point_step + pointcloud_.fields[0].offset], &p_side_x_, sizeof(float));
     memcpy(&pointcloud_.data[0 * pointcloud_.point_step + pointcloud_.fields[1].offset], &p_side_y_, sizeof(float));
@@ -44,8 +61,8 @@ void Bumper2PcNodelet::coreSensorCB(const kobuki_msgs::SensorState::ConstPtr& ms
     memcpy(&pointcloud_.data[0 * pointcloud_.point_step + pointcloud_.fields[1].offset], &P_INF_Y, sizeof(float));
   }
 
-  if ((msg->bumper & kobuki_msgs::SensorState::BUMPER_CENTRE) ||
-      (msg->cliff  & kobuki_msgs::SensorState::CLIFF_CENTRE))
+  if ((msg->bumper & kobuki_ros_interfaces::msg::SensorState::BUMPER_CENTRE) ||
+      (msg->cliff  & kobuki_ros_interfaces::msg::SensorState::CLIFF_CENTRE))
   {
     memcpy(&pointcloud_.data[1 * pointcloud_.point_step + pointcloud_.fields[0].offset], &pc_radius_, sizeof(float));
   }
@@ -54,8 +71,8 @@ void Bumper2PcNodelet::coreSensorCB(const kobuki_msgs::SensorState::ConstPtr& ms
     memcpy(&pointcloud_.data[1 * pointcloud_.point_step + pointcloud_.fields[0].offset], &P_INF_X, sizeof(float));
   }
 
-  if ((msg->bumper & kobuki_msgs::SensorState::BUMPER_RIGHT) ||
-      (msg->cliff  & kobuki_msgs::SensorState::CLIFF_RIGHT))
+  if ((msg->bumper & kobuki_ros_interfaces::msg::SensorState::BUMPER_RIGHT) ||
+      (msg->cliff  & kobuki_ros_interfaces::msg::SensorState::CLIFF_RIGHT))
   {
     memcpy(&pointcloud_.data[2 * pointcloud_.point_step + pointcloud_.fields[0].offset], &p_side_x_, sizeof(float));
     memcpy(&pointcloud_.data[2 * pointcloud_.point_step + pointcloud_.fields[1].offset], &n_side_y_, sizeof(float));
@@ -67,24 +84,58 @@ void Bumper2PcNodelet::coreSensorCB(const kobuki_msgs::SensorState::ConstPtr& ms
   }
 
   pointcloud_.header.stamp = msg->header.stamp;
-  pointcloud_pub_.publish(pointcloud_);
+  pointcloud_pub_->publish(pointcloud_);
 }
 
-void Bumper2PcNodelet::onInit()
+rcl_interfaces::msg::SetParametersResult Bumper2PcNode::paramChangeCallback(const std::vector<rclcpp::Parameter> & parameters)
 {
-  ros::NodeHandle nh = this->getPrivateNodeHandle();
+  rcl_interfaces::msg::SetParametersResult result;
+  result.successful = true;
 
-  // Bumper/cliff pointcloud distance to base frame; should be something like the robot radius plus
-  // costmap resolution plus an extra to cope with robot inertia. This is a bit tricky parameter: if
-  // it's too low, costmap will ignore this pointcloud (the robot footprint runs over the hit obstacle),
-  // but if it's too big, hit obstacles will be mapped too far from the robot and the navigation around
-  // them will probably fail.
-  std::string base_link_frame;
-  double r, h, angle;
-  nh.param("pointcloud_radius", r, 0.25); pc_radius_ = r;
-  nh.param("pointcloud_height", h, 0.04); pc_height_ = h;
-  nh.param("side_point_angle", angle, 0.34906585); 
-  nh.param<std::string>("base_link_frame", base_link_frame, "/base_link");
+  bool changed = false;
+  float radius = pc_radius_;
+  float height = pc_height_;
+  float angle = side_point_angle_;
+  std::string frame = base_link_frame_;
+
+  for (const rclcpp::Parameter & parameter : parameters)
+  {
+    if (parameter.get_name() == "pointcloud_radius")
+    {
+      radius = parameter.as_double();
+      changed = true;
+    }
+    else if (parameter.get_name() == "pointcloud_height")
+    {
+      height = parameter.as_double();
+      changed = true;
+    }
+    else if (parameter.get_name() == "side_point_angle")
+    {
+      angle = parameter.as_double();
+      changed = true;
+    }
+    else if (parameter.get_name() == "base_link_frame")
+    {
+      base_link_frame_ = parameter.as_string();
+      changed = true;
+    }
+  }
+
+  if (changed)
+  {
+    reconfigurePointCloud(radius, height, angle, frame);
+  }
+
+  return result;
+}
+
+void Bumper2PcNode::reconfigurePointCloud(float radius, float height, float angle, const std::string & base_link_frame)
+{
+  pc_radius_ = radius;
+  pc_height_ = height;
+  side_point_angle_ = angle;
+  base_link_frame_ = base_link_frame;
 
   // Lateral points x/y coordinates; we need to store float values to memcopy later
   p_side_x_ = + pc_radius_*sin(angle); // angle degrees from vertical
@@ -93,6 +144,33 @@ void Bumper2PcNodelet::onInit()
 
   // Prepare constant parts of the pointcloud message to be  published
   pointcloud_.header.frame_id = base_link_frame;
+
+  // z: constant elevation from base frame
+  memcpy(&pointcloud_.data[0 * pointcloud_.point_step + pointcloud_.fields[2].offset], &pc_height_, sizeof(float));
+  memcpy(&pointcloud_.data[1 * pointcloud_.point_step + pointcloud_.fields[2].offset], &pc_height_, sizeof(float));
+  memcpy(&pointcloud_.data[2 * pointcloud_.point_step + pointcloud_.fields[2].offset], &pc_height_, sizeof(float));
+
+  RCLCPP_INFO(get_logger(), "Bumper/cliff pointcloud configured at distance %f and height %f from base frame", pc_radius_, pc_height_);
+}
+
+Bumper2PcNode::Bumper2PcNode(const rclcpp::NodeOptions & options) : rclcpp::Node("kobuki_bumper2pc", options),
+      P_INF_X(+100*sin(0.34906585)),
+      P_INF_Y(+100*cos(0.34906585)),
+      N_INF_Y(-100*cos(0.34906585)),
+      ZERO(0.0), prev_bumper_(0), prev_cliff_(0)
+{
+  // Bumper/cliff pointcloud distance to base frame; should be something like the robot radius plus
+  // costmap resolution plus an extra to cope with robot inertia. This is a bit tricky parameter: if
+  // it's too low, costmap will ignore this pointcloud (the robot footprint runs over the hit obstacle),
+  // but if it's too big, hit obstacles will be mapped too far from the robot and the navigation around
+  // them will probably fail.
+
+  float radius = declare_parameter("pointcloud_radius", 0.25);
+  float height = declare_parameter("pointcloud_height", 0.04);
+  float angle = declare_parameter("side_point_angle", 0.34906585);
+  std::string base_link_frame = declare_parameter("base_link_frame", "base_link");
+
+  // Prepare constant parts of the pointcloud message to be published
   pointcloud_.width  = 3;
   pointcloud_.height = 1;
   pointcloud_.fields.resize(3);
@@ -108,7 +186,7 @@ void Bumper2PcNodelet::onInit()
   {
     pointcloud_.fields[d].count    = 1;
     pointcloud_.fields[d].offset   = offset;
-    pointcloud_.fields[d].datatype = sensor_msgs::PointField::FLOAT32;
+    pointcloud_.fields[d].datatype = sensor_msgs::msg::PointField::FLOAT32;
   }
 
   pointcloud_.point_step = offset;
@@ -123,18 +201,15 @@ void Bumper2PcNodelet::onInit()
   // y: always 0 for central bumper
   memcpy(&pointcloud_.data[1 * pointcloud_.point_step + pointcloud_.fields[1].offset], &ZERO, sizeof(float));
 
-  // z: constant elevation from base frame
-  memcpy(&pointcloud_.data[0 * pointcloud_.point_step + pointcloud_.fields[2].offset], &pc_height_, sizeof(float));
-  memcpy(&pointcloud_.data[1 * pointcloud_.point_step + pointcloud_.fields[2].offset], &pc_height_, sizeof(float));
-  memcpy(&pointcloud_.data[2 * pointcloud_.point_step + pointcloud_.fields[2].offset], &pc_height_, sizeof(float));
+  // Prepare configurable parts of the pointcloud message to be published
+  reconfigurePointCloud(radius, height, angle, base_link_frame);
 
-  pointcloud_pub_  = nh.advertise <sensor_msgs::PointCloud2> ("pointcloud", 10);
-  core_sensor_sub_ = nh.subscribe("core_sensors", 10, &Bumper2PcNodelet::coreSensorCB, this);
+  pointcloud_pub_  = create_publisher<sensor_msgs::msg::PointCloud2>("pointcloud", 10);
+  core_sensor_sub_ = create_subscription<kobuki_ros_interfaces::msg::SensorState>("core_sensors", rclcpp::QoS(10), std::bind(&Bumper2PcNode::coreSensorCB, this, std::placeholders::_1));
 
-  ROS_INFO("Bumper/cliff pointcloud configured at distance %f and height %f from base frame", pc_radius_, pc_height_);
+  param_change_handle_ = add_on_set_parameters_callback(std::bind(&Bumper2PcNode::paramChangeCallback, this, std::placeholders::_1));
 }
 
 } // namespace kobuki_bumper2pc
 
-
-PLUGINLIB_EXPORT_CLASS(kobuki_bumper2pc::Bumper2PcNodelet, nodelet::Nodelet);
+RCLCPP_COMPONENTS_REGISTER_NODE(kobuki_bumper2pc::Bumper2PcNode)
