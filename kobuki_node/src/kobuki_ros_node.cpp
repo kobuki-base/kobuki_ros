5835a4daed51f6a47d4b9f44dc7f0f53d7432666
Chris Lalancette <clalancette@openrobotics.org>
2022-02-07 21:33:03 -0500

    Remove unnecessary setvbuf from nodes. (#31)
    This hasn't been necessary since Eloquent, so just remove
    the unnecessary code.
    
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- kobuki_node/src/kobuki_ros_node.cpp
+++ kobuki_node/src/kobuki_ros_node.cpp
@@ -1,4 +1,3 @@
-#include <cstdio>
 #include <memory>
 
 #include <rclcpp/rclcpp.hpp>
@@ -7,9 +6,6 @@
 
 int main(int argc, char** argv)
 {
-  // Configures stdout stream for no buffering
-  setvbuf(stdout, nullptr, _IONBF, BUFSIZ);
-
   rclcpp::init(argc, argv);
 
   rclcpp::spin(std::make_shared<kobuki_node::KobukiRos>(rclcpp::NodeOptions()));
