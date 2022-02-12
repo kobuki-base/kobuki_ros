31ce9f180dbffb5ebfedaa922f51805dd51437f5
Chris Lalancette <clalancette@openrobotics.org>
2022-02-12 13:36:35 +0000

    Fix up style warnings.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- kobuki_description/launch/robot_description.launch.py
+++ kobuki_description/launch/robot_description.launch.py
@@ -1,15 +1,15 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition
-from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    
+
     urdf_path = PathJoinSubstitution(
-        [FindPackageShare("kobuki_description"), "urdf", "kobuki_standalone.urdf.xacro"]
+        [FindPackageShare('kobuki_description'), 'urdf', 'kobuki_standalone.urdf.xacro']
     )
 
     rviz_config_path = PathJoinSubstitution(
@@ -19,19 +19,19 @@ def generate_launch_description():
     return LaunchDescription([
 
         DeclareLaunchArgument(
-            name='urdf', 
+            name='urdf',
             default_value=urdf_path,
             description='URDF path'
         ),
-        
+
         DeclareLaunchArgument(
-            name='use_sim_time', 
+            name='use_sim_time',
             default_value='false',
             description='Use simulation time'
         ),
 
         DeclareLaunchArgument(
-            name='rviz', 
+            name='rviz',
             default_value='false',
             description='Run rviz'
         ),
@@ -62,7 +62,7 @@ def generate_launch_description():
             name='rviz2',
             output='screen',
             arguments=['-d', rviz_config_path],
-            condition=IfCondition(LaunchConfiguration("rviz")),
+            condition=IfCondition(LaunchConfiguration('rviz')),
             parameters=[{
                 'use_sim_time': LaunchConfiguration('use_sim_time')
             }]
