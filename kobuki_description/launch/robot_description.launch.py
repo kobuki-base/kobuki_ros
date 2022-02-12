af28642a8fe573558447322a8e3309a3a4024ee3
wn1980 <waipotn@hotmail.com>
2022-02-12 14:44:27 +0700

    updated kobuki_description and kobuki_auto_docking

---
+++ kobuki_description/launch/robot_description.launch.py
@@ -0,0 +1,70 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    
+    urdf_path = PathJoinSubstitution(
+        [FindPackageShare("kobuki_description"), "urdf", "kobuki_standalone.urdf.xacro"]
+    )
+
+    rviz_config_path = PathJoinSubstitution(
+        [FindPackageShare('kobuki_description'), 'rviz', 'model.rviz']
+    )
+
+    return LaunchDescription([
+
+        DeclareLaunchArgument(
+            name='urdf', 
+            default_value=urdf_path,
+            description='URDF path'
+        ),
+        
+        DeclareLaunchArgument(
+            name='use_sim_time', 
+            default_value='false',
+            description='Use simulation time'
+        ),
+
+        DeclareLaunchArgument(
+            name='rviz', 
+            default_value='false',
+            description='Run rviz'
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'use_sim_time': LaunchConfiguration('use_sim_time'),
+                'robot_description': Command(['xacro ', LaunchConfiguration('urdf')])
+            }]
+        ),
+
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            parameters=[{
+                'use_sim_time': LaunchConfiguration('use_sim_time')
+            }]
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', rviz_config_path],
+            condition=IfCondition(LaunchConfiguration("rviz")),
+            parameters=[{
+                'use_sim_time': LaunchConfiguration('use_sim_time')
+            }]
+        ),
+    ])
